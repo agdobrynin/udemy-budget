72427bdedbf8f2f2ce1321f7192526fdc2ae2cc8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-02-27 20:12:31 +0400

    Refactoring UI element.

--- src/components/BudgetFormItem.vue
+++ src/components/BudgetFormItem.vue
@@ -1,9 +1,17 @@
 <template>
   <ElCard header="Добавить новый элемент бюджета">
-    <ElForm :model="formData" :rules="rules" label-width="120px" ref="ruleForm" @submit.prevent="onSave">
+    <ElForm
+        :model="formData"
+        :rules="rules"
+        label-width="120px"
+        ref="ruleForm"
+        @submit.native.prevent="onSave">
       <ElFormItem label="Тип" prop="type">
         <ElSelect class="type-select" v-model="formData.type">
-          <ElOption v-for="(typeTitle, key) in typeOfBudgetTitles" :key="key" :label="typeTitle" :value="typeTitle"/>
+          <ElOption v-for="(typeTitle, key) in typeOfBudgetTitles"
+                    :key="key"
+                    :label="typeTitle"
+                    :value="typeTitle"/>
         </ElSelect>
       </ElFormItem>
       <ElFormItem label="Комментарий" prop="comment">
@@ -12,7 +20,7 @@
       <ElFormItem label="Сумма" prop="value">
         <ElInput v-model.number="formData.value"></ElInput>
       </ElFormItem>
-      <ElButton type="primary">Добавить</ElButton>
+      <ElButton native-type="submit" type="primary">Добавить</ElButton>
     </ElForm>
   </ElCard>
 </template>
