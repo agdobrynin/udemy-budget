1af894524079136c252e09d558affb39ef62a68c
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-02-27 13:38:56 +0400

    BudgetListItem new component

--- src/components/BudgetList.vue
+++ src/components/BudgetList.vue
@@ -2,11 +2,11 @@
   <div class="budget-list-wrap">
     <ElCard :header="header">
       <template v-if="!isEmpty">
-        <div class="list-item" v-for="item in list" :key="item.id">
-          <span class="comment">{{ item.comment }}</span>
-          <span class="value">{{ item.value }}</span>
-          <ElButton type="danger" @click.prevent="deleteItem(item.id)">удалить</ElButton>
-        </div>
+        <BudgetListItem v-for="item in list"
+                        :key="item.id"
+                        :budget-item="item"
+                        @deleteBudgetItem="deleteBudgetItem">
+        </BudgetListItem>
       </template>
       <ElAlert v-else type="info" show-icon :closable="false">Записей нет.</ElAlert>
     </ElCard>
@@ -15,8 +15,15 @@
 
 <script>
 
+import BudgetListItem from "@/components/BudgetListItem";
+
 export default {
   name: "BudgetList",
+
+  components: {
+    BudgetListItem
+  },
+
   props: {
     list: {
       /** @type{Array<BudgetItemOutcome|BudgetItemIncome>} */
@@ -24,16 +31,19 @@ export default {
       default: () => ({}),
     }
   },
+
   data: () => ({
     header: "Бюджет",
   }),
+
   computed: {
     isEmpty() {
       return !Object.keys(this.list).length;
     }
   },
+
   methods: {
-    deleteItem(id) {
+    deleteBudgetItem(id) {
       this.$emit("deleteItem", id);
     }
   },
@@ -44,17 +54,4 @@ export default {
 .budget-list-wrap {
   margin: auto;
 }
-
-.list-item {
-  display: flex;
-  align-items: center;
-  padding: 10px 15px;
-}
-
-.value {
-  font-weight: bold;
-  margin-left: auto;
-  margin-right: 20px;
-}
-
 </style>
---
+++ src/components/BudgetListItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="list-item">
+    <span class="comment">{{ BudgetItem.comment }}</span>
+    <span class="value">{{ BudgetItem.value }}</span>
+    <ElButton type="danger" @click.prevent="deleteItem(BudgetItem.id)">удалить</ElButton>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BudgetListItem",
+
+  props: {
+    BudgetItem: {
+      /** @type{BudgetItemOutcome|BudgetItemIncome} */
+      type: Object,
+      default: () => ({}),
+    }
+  },
+
+  methods: {
+    deleteItem(id) {
+      this.$emit("deleteBudgetItem", id);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+
+.value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
+
+</style>
