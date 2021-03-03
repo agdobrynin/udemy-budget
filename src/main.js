984ce53924dba1a9e9ade5ed2dbc404490a2d0a2
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-03-03 18:35:00 +0400

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/plugins/elements";
 import "@/plugins/elements/css";
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount("#app");
