dc3f796cc021087b330cfb71eda124be84f908b8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-03-04 22:55:07 +0400

    Fix unit tests.

--- tests/unit/Entity/BudgetItemOutcome.spec.js
+++ tests/unit/Entity/BudgetItemOutcome.spec.js
@@ -2,35 +2,46 @@ const {BudgetItemOutcome} = require("@/Entity/BudgetItemOutcome");
 
 describe("BudgetItemOutcome entity", () => {
     it("Init with empty constructor", () => {
-        const budgetItemOutcome = new BudgetItemOutcome();
+        const budgetItem = new BudgetItemOutcome();
 
-        expect(budgetItemOutcome.id).toMatch(/([a-z0-9]{10,})/i);
-        expect(budgetItemOutcome.typeTitle).toEqual("Расход");
-        expect(budgetItemOutcome.comment).toEqual(undefined);
-        expect(budgetItemOutcome.value).toEqual(NaN);
+        expect(budgetItem.id).toMatch(/([a-z0-9]{10,})/i);
+        expect(budgetItem.typeTitle).toEqual("Расход");
+        expect(budgetItem.comment).toEqual(undefined);
+        expect(budgetItem.value).toEqual(NaN);
     });
 
     it("Init with not empty constructor", () => {
         const comment = "Привет";
         const value = 150;
-        const budgetIncome = new BudgetItemOutcome(comment, value);
+        const budgetItem = new BudgetItemOutcome(comment, value);
 
-        expect(budgetIncome.comment).toEqual(comment);
-        expect(budgetIncome.value).toEqual(-value);
+        expect(budgetItem.comment).toEqual(comment);
+        expect(budgetItem.value).toEqual(-value);
+    });
+
+    it("Init with constructor and id", () => {
+        const comment = "Привет";
+        const value = 150;
+        const id = "KLV1FBTZL0TW9";
+        const budgetItem = new BudgetItemOutcome(comment, value, id);
+
+        expect(budgetItem.comment).toEqual(comment);
+        expect(budgetItem.value).toEqual(-value);
+        expect(budgetItem.id).toEqual(id);
     });
 
     it("Set negative value", () => {
         const value = -150;
-        const budgetIncome = new BudgetItemOutcome("", value);
+        const budgetItem = new BudgetItemOutcome("", value);
 
-        expect(budgetIncome.value).toEqual(value);
+        expect(budgetItem.value).toEqual(value);
     });
 
     it("Set positive value", () => {
         const value = 150;
-        const budgetIncome = new BudgetItemOutcome("", value);
+        const budgetItem = new BudgetItemOutcome("", value);
 
-        expect(budgetIncome.value).toEqual(-value);
+        expect(budgetItem.value).toEqual(-value);
     });
 
 });
--- tests/unit/sore/parseStorageItems.spec.js
+++ tests/unit/sore/parseStorageItems.spec.js
@@ -1,7 +1,5 @@
 import {parseStorageItems} from "@/store/budget/persistedstate";
 import storageItems from "../storageItems.json";
-import {BudgetItemIncome} from "@/Entity/BudgetItemIncome";
-import {BudgetItemOutcome} from "@/Entity/BudgetItemOutcome";
 
 describe("Vuex-persistedstate Test", () => {
     it("Json parser from localStorage to BudgetItems", () => {
@@ -9,7 +7,7 @@ describe("Vuex-persistedstate Test", () => {
         const {budget:{items}} = parseStorageItems(sourceString);
 
         Object.values(items).forEach((budgetItem) => {
-            expect([BudgetItemIncome.name, BudgetItemOutcome.name].includes(budgetItem.constructor.name)).toBeTruthy();
+            expect(["BudgetItemIncome", "BudgetItemOutcome"].includes(budgetItem.className)).toBeTruthy();
         });
     });
 });
