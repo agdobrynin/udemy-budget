11a6414287828eda26a85ee405f96897ce0224ae
Alexey Dobrynin <ag.dobrynin@gmail.com>
2021-03-05 13:46:23 +0400

    bug(webpack): Not change Class name when the file is minified

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+/**
+ * Class name change when the file is minified (add section chainWebpack)
+ * https://github.com/vuejs/vue-cli/issues/1118#issuecomment-592743751
+ */
 module.exports = {
-    publicPath: './'
+    publicPath: './',
+    chainWebpack: config => {
+        config.optimization
+            .minimizer('terser')
+            .tap(args => {
+                const { terserOptions } = args[0]
+                terserOptions.keep_classnames = true
+                terserOptions.keep_fnames = true
+                return args
+            })
+    },
 }
